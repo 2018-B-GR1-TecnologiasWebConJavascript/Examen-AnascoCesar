a4760565af2ca40415c251068e2b2a226c896fbb
Washington Añasco <wacho_jax@hotmail.com>
2018-11-30 18:04:51 -0500

    Update main.ts

--- 2018-b-gr1-master/examen-01/main.ts
+++ 2018-b-gr1-master/examen-01/main.ts
@@ -7,25 +7,6 @@ const mergeMap = require('rxjs/operators').mergeMap;
 const map = require('rxjs/operators').map;
 //const bdd = require('rxjs/operators')
 
-class People {
-    name: string;
-    height: string;
-    mass: string;
-    hair_color: string;
-    skin_color: string;
-    eye_color: string;
-    birth_year: string;
-    gender: string;
-    homeworld: string;
-    films: string[] = [];
-    species: string[] = [];
-    vehicles: string[] = [];
-    starships: string[] = [];
-    created: string;
-    edited: string;
-    url: string
-}
-
 const pokemon = [
         {
             "abilities": ['comer','dormir'],
